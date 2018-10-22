370133f3e1b8a5b842ece38d602096dff74ab7ec
Carsten Andrzejak <carsten.andrzejak@local.host>
2018-10-22 02:38:18 +0200

    [create] html/index.php, [del] html/index.html, webocton.data

---
+++ html/index.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>C.A. Home</title>
+
+    <meta charset="UTF-8">
+    <meta name="description" content="wein Kleines Intranet">
+    <meta name="author" content="Carsten Andrzejak">
+    <meta name="keywords" content="Raspberry pi, Heimautomatisierung">
+
+    <link href="favicon.ico" type="image/x-icon" rel="shortcut icon">
+</head>
+
+<body>
+    <header>
+        <h1>Wilkommen</h1>
+        <h2>auf C.A. HOME</h2>
+    </header>
+</body>
+</html>
