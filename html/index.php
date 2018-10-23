b69b52fc823c3f835da82f4cbdbd0169ad6acfe3
Carsten Andrzejak <CarAndMore@github.com>
2018-10-23 15:43:27 +0200

    Version 6: erste *.js functions     - hide news

--- html/index.php
+++ html/index.php
@@ -8,30 +8,49 @@
 <!DOCTYPE html>
 <html>
 
-<head>
-    <title>C.A. Home</title>
-
-    <meta charset="UTF-8">
-    <meta name="description" content="Mein Kleines Intranet">
-    <meta name="author" content="Carsten Andrzejak">
-    <meta name="keywords" content="Raspberry pi, Heimautomatisierung">
-
-    <link href="favicon.ico" type="image/x-icon" rel="shortcut icon">
-    <link href="styles/thema.css" type="text/css" rel="stylesheet" />
-</head>
-
-<body>
-    <header>
-        <h1>Wilkommen</h1>
-        <h2>auf C.A. HOME</h2>
-    </header>
-    <section>
-        <h3>
-            Hallo Welt
-        </h3>
-    </section>
-    <footer>
-        <p>no impressum; no AGB's; F*ck DSGVO'</p>
-    </footer>
-</body>
+    <head>
+        <title>C.A. Home</title>
+    
+        <meta charset="UTF-8">
+        <meta name="description" content="Mein Kleines Intranet">
+        <meta name="author" content="Carsten Andrzejak">
+        <meta name="keywords" content="Raspberry pi, Heimautomatisierung">
+    
+        <link href="favicon.ico" type="image/x-icon" rel="shortcut icon">
+        <link href="styles/thema.css" type="text/css" rel="stylesheet" />
+        
+        <script language="javascript" type="text/javascript" src="script/jquery.js"></script>
+        <script language="javascript" type="text/javascript" src="script/my_js.js"></script>
+    </head>
+    
+    <body>
+        <header>
+            <div id="kopf">
+                <h1>Wilkommen</h1>
+                <h2>auf C.A. HOME</h2>
+            </div>
+            <div id="news" hidden="hidden">
+                Hier kommt bald der gro&szlig;e Wald!
+            </div>
+        </header>
+        <section>
+            <h3>
+                Hallo Welt
+            </h3>
+            <article>
+                <h4 class="headline">
+                    &Uuml;berschrift
+                </h4>
+                <p>
+                    hier k&ouml;nnte Ihr Text Stehen.  
+                    <br />
+                    tut er aber leider nicht. und das ist sehr schade.
+                    warum tut er das nicht? los sprich!
+                </p>
+            </article>
+        </section>
+        <footer>
+            <p>no impressum; no AGB's; F*ck DSGVO'</p>
+        </footer>
+    </body>
 </html>
---
+++ html/script/my_js.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $( "#kopf" ).click(function() {
+        $("#news").toggle("slow");
+    });
+});
